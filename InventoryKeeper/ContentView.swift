0135d78191e638fbf8ddc7edfd36e6615fdd3769
Lightbekon <lightbekon@gmail.com>
2022-06-26 16:58:35 +1000

    Initial Commit

---
+++ InventoryKeeper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InventoryKeeper
+//
+//  Created by william Vise on 26/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InventoryKeeper/InventoryKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryKeeperApp.swift
+//  InventoryKeeper
+//
+//  Created by william Vise on 26/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct InventoryKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
